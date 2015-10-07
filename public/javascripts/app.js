eb1f22b94d2198f62732008fef360a6970230439
ianh <ian4harrison@gmail.com>
2015-10-07 21:40:14 +0100

    Adding angular list

---
+++ public/javascripts/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by PCDubh on 07/10/2015.
+ */
+
+//'use strict';
+//
+//var siteMonitorApp = angular.module('siteMonitorApp', ['siteListControllers']);
+//
+////siteMonitorApp.config(['$routeProvider',
+////    function($routeProvider){
+////        console.log("ROUTEPROVIDERCONFIG");
+////    }
+////]);
+
